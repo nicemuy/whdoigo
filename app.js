6da492992f5227a0447168cf4175b6ee27bc00db
LeeYoungHak <nicemuy@gmail.com>
2013-07-01 01:59:48 +0900

    ctx 변경

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
-app.use(app.router);
+app.use('/whdoigo',app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only

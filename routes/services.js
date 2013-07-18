732bd4ff87315f06e8b289631d1d9e737c736116
LeeYoungHak <nicemuy@gmail.com>
2013-07-19 01:25:45 +0900

    share pictureddd

--- routes/services.js
+++ routes/services.js
@@ -309,10 +309,12 @@ exports.sharepicture = function(req, res){
     }
     pool.getConnection(function(err, connection) {
         connection.query( 'insert into shared(c_id,picture,picture_memo,up_date) values(?,?,?,now())', [req.body.c_id ,baseUrl+path.basename(req.files.picture.path),req.body.picture_memo], function(err, rows) {
-            connection.end();
-            res.send(200,'true');
-            console.log('push');
-            gcm_push(req.body.party_id,userid);
+            connection.query('UPDATE member_party SET isnew = true WHERE party_id = ?',[req.body.party_id],function(err, rows){
+                connection.end();
+                res.send(200,'true');
+                console.log('push');
+                gcm_push(req.body.party_id,userid);
+            });
         });
     });
 };
